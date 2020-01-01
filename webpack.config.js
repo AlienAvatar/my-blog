31795809678115da8e3a4e2c34cc4d45098e6ad5
Avatar <dxf767808421@163.com>
2020-01-01 14:59:50 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',               // 入口文件
+    output: {
+        filename:'bundle.js',
+        path:path.resolve('dist'),
+    }, // 出口文件
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // use: ['style-loader', 'css-loader']
+                use: ExtractTextWebpackPlugin.extract({
+                    // 将css用link的方式引入就不再需要style-loader了
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,    // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+                            outputPath: 'images/'   // 图片打包后存放的目录
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new ExtractTextWebpackPlugin('css/style.css'),
+        new CleanWebpackPlugin()
+    ]
+};
