c55035a3f69b6069d8db4c5818aba02cbd1f7a61
react-blog <dxf767808421@163.com>
2020-01-28 23:48:17 +0800

    1.实现登录保持状态 2.点击文章，跳入文章详情页面 3.重构了文章编辑页面，使用draft.js 4.添加setting页面，评论正常显示 5.部署服务器 6.增加了supoort页面

--- src/Constant/loginConstant.js
+++ src/Constant/loginConstant.js
@@ -2,7 +2,7 @@ import {notification} from "antd";
 
 export const local = {
     // url : 'http://localhost:8081/api'
-    url:"http://111.229.76.149/api"
+    url:"http://111.229.76.149:8081/api"
 };
 
 export const openLoginNotificationWithIcon = (type,title,msg) => {
